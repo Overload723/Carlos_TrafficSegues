9c0df80e6aef15ca3df0aab97951cc7f28a0368f
Carlos <carmez.gomlos@gmail.com>
2021-04-01 11:13:08 -0400

    Initial Commit

---
+++ Carlos_TrafficSegues/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Carlos_TrafficSegues
+//
+//  Created by Carlos Gomez on 4/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
